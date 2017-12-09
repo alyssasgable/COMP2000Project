2ade44c9a5623674c7bc4aa33f177033975c7603
Alyssa <alyssasgable@gmail.com>
2017-12-08 21:54:37 -0600

    edited login function

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function userLogin() {
     var email = prompt("Please enter your Auburn University email address.");
-    if (email != null && email.includes("@auburn.edu")) {
+    if (email != null && email.includes("auburn")) {
         window.open('resources.html', '_self');
     } else {
       alert("Invalid email address.");
